0062d725c47f6f52924f25cd05b95f725303b384
SharpFox <roman.bb.vishnevsky@gmail.com>
2017-08-30 12:33:33 +1000

    #8 Итерация по массивам правка №1

--- index.php
+++ index.php
@@ -13,37 +13,37 @@ $lot_time_remaining = gmdate("H:i", ($tomorrow - $now));
 
 $goodsCategory = ["Доски и лыжи","Крепления","Ботинки","Одежда","Инструменты","Разное"];
 $goodsContents = [
-    0 => [
+    [
         'name' => "2014 Rossignol District Snowboard",
         'category' => "Доски и лыжи",
         'cost' => 10999,
         'url' => "img/lot-1.jpg"
     ],
-    1 => [
+    [
         'name' => "DC Ply Mens 2016/2017 Snowboard",
         'category' => "Доски и лыжи",
         'cost' => 159999,
         'url' => "img/lot-2.jpg"
     ],
-    2 => [
+    [
         'name' => "Крепления Union Contact Pro 2015 года размер L/XL",
         'category' => "Крепления",
         'cost' => 8000,
         'url' => "img/lot-3.jpg"
     ],
-    3 => [
+    [
         'name' => "Ботинки для сноуборда DC Mutiny Charocal",
         'category' => "Ботинки",
         'cost' => 10999,
         'url' => "img/lot-4.jpg"
     ],
-    4 => [
+    [
         'name' => "Куртка для сноуборда DC Mutiny Charocal",
         'category' => "Одежда",
         'cost' => 7500,
         'url' => "img/lot-5.jpg"
     ],
-    5 => [
+    [
         'name' => "Маска Oakley Canopy",
         'category' => "Разное",
         'cost' => 5400,
@@ -51,8 +51,8 @@ $goodsContents = [
     ]
 ];
 
-
 ?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>

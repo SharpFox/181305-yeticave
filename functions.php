264d7f4ba84e800877ec8fc3ddec4490db759951
SharpFox <roman.bb.vishnevsky@gmail.com>
2017-09-03 02:57:10 +1000

    #10 Шаблонизация проекта

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php 
+function renderTemplate($path, $varArray) {
+
+    if (!file_exists($path)) {
+        return "";
+    }
+
+    ob_start('ob_gzhandler');
+
+    extract($varArray, EXTR_SKIP);
+    require_once $path;
+
+    return ob_get_clean(); 
+}
+?>

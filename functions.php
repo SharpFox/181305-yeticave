1f66d0fd34a7c507c580188c59959b35a1ee8746
SharpFox <roman.bb.vishnevsky@gmail.com>
2017-09-03 04:33:33 +1000

    #10 Шаблонизация проекта, правка №3

--- functions.php
+++ functions.php
@@ -3,12 +3,11 @@ function renderTemplate($path, $varArray) {
 
     if (!file_exists($path)) {
         return "";
-    }
-   
-    require_once $path;
+    }    
 
     ob_start('ob_gzhandler');
     extract($varArray, EXTR_SKIP);
+    require_once $path;
     
     return ob_get_clean(); 
 }

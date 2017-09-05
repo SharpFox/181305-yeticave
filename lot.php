2223f48167c28f609a6ec477660cab05768dde28
SharpFox <roman.bb.vishnevsky@gmail.com>
2017-09-06 04:05:00 +1000

    #12 Получение данных из форм #2

--- lot.php
+++ lot.php
@@ -1,11 +1,5 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-<<<<<<< HEAD
-=======
-
-require_once('functions.php');
-require_once('lot_data.php');
->>>>>>> 4b0556c03052e99cf0cc50a4a6ffd940c881f01e
 
 require_once('functions.php');
 require_once('data.php');
@@ -13,18 +7,8 @@ require_once('data.php');
 $goodsItem = isset($_GET['id']) ? $_GET['id'] : null;
 toPrintErrorInfo($goodsItem, $goodsContent);
 
-<<<<<<< HEAD
 $navVar = ['goodsCategory' => $goodsCategory];
 $navContent = toRenderTemplate('nav.php', $navVar);
-=======
-if (!array_key_exists($goodsItem, $goodsContents)) {
-    header('HTTP/1.1 404 Not Found');
-    print('Ошибка 404');
-
-    die();
-}
-?>
->>>>>>> 4b0556c03052e99cf0cc50a4a6ffd940c881f01e
 
 $lotVar = [ 
     'goodsContent' => $goodsContent,

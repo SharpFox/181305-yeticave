1c85eeefb099da39d66a1966557f9ed50693ff6d
SharpFox <roman.bb.vishnevsky@gmail.com>
2017-09-08 07:10:25 +1000

    #12 Получение данных из форм #4

--- templates/add.php
+++ templates/add.php
@@ -1,25 +1,13 @@
 <?=$navigationMenu;?>
-<?php if (!empty($errors)): ?>
-<form class="form form--invalid form--add-lot container" action="add.php" method="post" enctype="multipart/form-data">
-<?php else: ?>
-<form class="form form--add-lot container" action="add.php" method="post" enctype="multipart/form-data">
-<?php endif; ?>
+<form class="form form--add-lot container <?=!empty($errors) ? 'form--invalid' : '';?>" action="add.php" method="post" enctype="multipart/form-data">
   <h2>Добавление лота</h2>
     <div class="form__container-two">
-      <?php if (in_array('lot-name', $errors)): ?>
-      <div class="form__item form__item--invalid">
-      <?php else: ?>
-      <div class="form__item">
-      <?php endif; ?>
+      <div class="form__item <?=key_exists('lot-name', $errors) ? 'form__item--invalid' : '';?>">
        <label for="lot-name">Наименование</label>
        <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" required>
        <span class="form__error"></span>
       </div>
-      <?php if (in_array('category', $errors)): ?>
-      <div class="form__item form__item--invalid">
-      <?php else: ?>
-      <div class="form__item">
-      <?php endif; ?>
+      <div class="form__item <?=key_exists('category', $errors) ? 'form__item--invalid' : '';?>">
         <label for="category">Категория</label>
         <select id="category" name="category" required>
           <option>Выберите категорию</option>
@@ -30,16 +18,17 @@
         <span class="form__error"></span>
       </div>
     </div>
-    <?php if (in_array('message', $errors)): ?>
-    <div class="form__item form__item--wide form__item--invalid">
-    <?php else: ?>
-    <div class="form__item form__item--wide">
-    <?php endif; ?>
+    <div class="form__item form__item--wide <?=key_exists('message', $errors) ? 'form__item--invalid' : '';?>">
       <label for="message">Описание</label>
       <textarea id="message" name="message" placeholder="Напишите описание лота" required></textarea>
       <span class="form__error"></span>
     </div>
-    <div class="form__item form__item--file"> <!-- form__item--uploaded -->
+
+    <?php if (in_array('lot_photo', $errors)) : ?>
+    <div class="form__item form__item--file form__item--invalid"> <!--form__item--uploaded-->
+    <?php else: ?>
+    <div class="form__item form__item--file">
+    <?php endif; ?>
       <label>Изображение</label>
       <div class="preview">
         <button class="preview__remove" type="button">x</button>
@@ -55,29 +44,17 @@
       </div>
     </div>
     <div class="form__container-three">
-      <?php if (in_array('lot-rate', $errors)): ?>
-      <div class="form__item form__item--small form__item--invalid">
-      <?php else: ?>
-      <div class="form__item form__item--small">
-      <?php endif; ?>
+      <div class="form__item form__item--small <?=key_exists('lot-rate', $errors) ? 'form__item--invalid' : '';?>">
         <label for="lot-rate">Начальная цена</label>
         <input id="lot-rate" type="number" name="lot-rate" placeholder="0" required>
         <span class="form__error"></span>
       </div>
-      <?php if (in_array('lot-step', $errors)): ?>
-      <div class="form__item form__item--small form__item--invalid">
-      <?php else: ?>
-      <div class="form__item form__item--small">
-      <?php endif; ?>
+        <div class="form__item form__item--small <?=key_exists('lot-step', $errors) ? 'form__item--invalid' : '';?>">
         <label for="lot-step">Шаг ставки</label>
         <input id="lot-step" type="number" name="lot-step" placeholder="0" required>
         <span class="form__error"></span>
       </div>
-      <?php if (in_array('lot-date', $errors)): ?>
-      <div class="form__item form__item--small form__item--invalid">
-      <?php else: ?>
-      <div class="form__item form__item--small">
-      <?php endif; ?>
+      <div class="form__item form__item--small <?=key_exists('lot-date', $errors) ? 'form__item--invalid' : '';?>">
       <div class="form__item">
         <label for="lot-date">Дата завершения</label>
         <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="20.05.2017" required>

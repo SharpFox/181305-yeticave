8800429b17f7be9afb571e8f0bbfe27dadb4a176
Вишневский Роман <vishnevsky_r@dalzaso.local>
2017-09-22 15:04:22 +1000

    #16 Пишем SQL-запросы #1

--- schema.sql
+++ schema.sql
@@ -5,10 +5,10 @@ CREATE TABLE users (
 	id 				    INT UNSIGNED 	AUTO_INCREMENT PRIMARY KEY,
 	email 			  CHAR(254) 		NOT NULL,
 	passwordHash 	CHAR(80) 		NOT NULL,
-	createdTime 	DATETIME,
 	name 				  CHAR(40) 		  NOT NULL,
-	avatarUrl 		CHAR(150),
-	contacts 		  CHAR(200) 		NOT NULL
+  avatarUrl 		CHAR(150),
+	contacts 		  CHAR(200) 		NOT NULL,
+	createdTime 	DATETIME
 );
 
 CREATE TABLE categories (
@@ -18,23 +18,23 @@ CREATE TABLE categories (
 
 CREATE TABLE lots (
   id 					    INT UNSIGNED 	AUTO_INCREMENT PRIMARY KEY,
-  createdTime 		DATETIME,
   name 				    CHAR(150) 		NOT NULL,
-  categoryId 		  INT UNSIGNED,
   cost 				    FLOAT 			  NOT NULL,
   imgUrl 			    CHAR(150) 		NOT NULL,
   description 		TEXT(600) 		NOT NULL,
-  timeRemaining 	DATETIME 		  NOT NULL,
+  endTime 	DATETIME 		  NOT NULL,
   step 				    INT UNSIGNED 	NOT NULL,
   quantityBets    INT UNSIGNED  NOT NULL,
+  categoryId 		  INT UNSIGNED,
   authorId 		    INT UNSIGNED,
-  winnerId 		    INT UNSIGNED
+  winnerId 		    INT UNSIGNED,
+  createdTime 		DATETIME
 );
 
 CREATE TABLE bets (
   id 				    INT UNSIGNED  AUTO_INCREMENT PRIMARY KEY,
   createdTime   DATETIME,
-  timeRemaining DATETIME 		  NOT NULL,
+  endTime       DATETIME 		  NOT NULL,
   cost 			    FLOAT         NOT NULL,
   userId 		    INT UNSIGNED,
   lotId 			  INT UNSIGNED

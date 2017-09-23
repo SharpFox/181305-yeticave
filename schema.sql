8e663f3eb8472ca31e17863b8299255685978c2f
SharpFox <roman.bb.vishnevsky@gmail.com>
2017-09-23 13:04:48 +1000

    #17 Подключение к MySQL #2

--- schema.sql
+++ schema.sql
@@ -3,26 +3,26 @@ USE yeticave_181305;
 
 CREATE TABLE users (
 	id 				    INT UNSIGNED 	AUTO_INCREMENT PRIMARY KEY,
-	email 			  CHAR(254) 		NOT NULL,
-	passwordHash 	CHAR(80) 		NOT NULL,
-	name 				  CHAR(40) 		  NOT NULL,
-  avatarUrl 		CHAR(150),
-	contacts 		  CHAR(200) 		NOT NULL,
+	email 			  VARCHAR (254) NOT NULL,
+	passwordHash 	VARCHAR(60) 	NOT NULL,
+	name 				  VARCHAR(40) 	NOT NULL,
+  avatarUrl 		VARCHAR(150),
+	contacts 		  VARCHAR(200) 	NOT NULL,
 	createdTime 	DATETIME
 );
 
 CREATE TABLE categories (
   id 		INT UNSIGNED 	AUTO_INCREMENT PRIMARY KEY,
-  name 	CHAR(40) 		  NOT NULL
+  name 	VARCHAR(40) 	NOT NULL
 );
 
 CREATE TABLE lots (
   id 					    INT UNSIGNED 	AUTO_INCREMENT PRIMARY KEY,
-  name 				    CHAR(150) 		NOT NULL,
-  cost 				    FLOAT 			  NOT NULL,
-  imgUrl 			    CHAR(150) 		NOT NULL,
+  name 				    VARCHAR(150) 	NOT NULL,
+  cost 				    DOUBLE 			  NOT NULL,
+  imgUrl 			    VARCHAR(150) 	NOT NULL,
   description 		TEXT(600) 		NOT NULL,
-  endTime 	DATETIME 		  NOT NULL,
+  endTime 	      DATETIME 		  NOT NULL,
   step 				    INT UNSIGNED 	NOT NULL,
   quantityBets    INT UNSIGNED  NOT NULL,
   categoryId 		  INT UNSIGNED,
@@ -35,7 +35,7 @@ CREATE TABLE bets (
   id 				    INT UNSIGNED  AUTO_INCREMENT PRIMARY KEY,
   createdTime   DATETIME,
   endTime       DATETIME 		  NOT NULL,
-  cost 			    FLOAT         NOT NULL,
+  cost 			    DOUBLE        NOT NULL,
   userId 		    INT UNSIGNED,
   lotId 			  INT UNSIGNED
 );
